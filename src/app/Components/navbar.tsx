12b3f64ad0494e0dc91e152b937f7e599e47d907
ViduminiArdarsha <ghvidumini@gmail.com>
2025-02-22 19:20:08 -1200

    navar initial

---
+++ src/app/Components/navbar.tsx
@@ -0,0 +1,23 @@
+import { BiDownload } from "react-icons/bi";
+
+function navbar(){
+    return(
+        <section className="bg-white px-20">
+                <nav className="py-8 mb-10 flex justify-between">
+                  <h2 className="py-3">Vidumini's Portfolio</h2>
+                    <ul className="flex justify-between px-15 py-3 gap-10">
+                      <li>Home</li>
+                      <li>About</li>
+                      <li>Projects</li>
+                      <li>Contact</li>
+                    </ul>
+        
+                  <div className="bg-gradient-to-r from-indigo-500 to-blue-600 px-5 py-3 rounded-md text-white flex justify-between">
+                    <BiDownload className="cursor-pointer text-2xl"/>
+                    <a href="#" className="ml-3">Download CV</a>
+                  </div>
+                </nav>
+              </section>
+    );
+}
+export default navbar;
