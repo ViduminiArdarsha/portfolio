16029d1e8afdeb8579b1dbe27d4c67a8c4d393b8
ViduminiArdarsha <ghvidumini@gmail.com>
2025-02-24 02:15:16 -1200

    navbar added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import useSWR from "swr";
-import navbar from "./Components/navbar";
+import Navbar from "./Components/navbar";
 import Image from 'next/image';
+import { BiPhoneCall } from "react-icons/bi";
 
 
 export default function Home() {
@@ -9,20 +10,25 @@ export default function Home() {
     <main>
       <title>My Portfolio</title>
 
-     <navbar/> 
+     <Navbar/>
      <section className="flex justify-center gap-40 pt-4 px-20">
        <div className="text-center">
          <h1 className="font-semibold text-5xl m-8 text-primary"> Vidumini Ardarsha</h1>
          <h2 className="font-semibold text-7xl m-8 text-accent pt-8">UI/UX Designer</h2>
-       </div>
-       <div>
-         <Image src="/profilePhoto.jpg"
-         alt="Profile Picture"
-         width={150}
-         height={150}
-         />
+         <div className="border-2 border-accent rounded-lg px-6 py-3 text-accent flex justify-center w-30">
+          <BiPhoneCall className="cursor-pointer text-2xl"/>
+          <a href="#" className="ml-3">Contact Me</a>
+         </div>
        </div>
        
+       <div className="relative my-auto bg-gradient-to-b from-accent rounded-full w-40 h-40 overflow-hidden">
+         <Image src="/profilePhoto.jpg"
+         alt="Profile Picture"
+        layout="fill"
+        
+         objectFit="cover"
+         />
+       </div>  
      </section>
         <a href="/projects">Projects</a>
      <section>
