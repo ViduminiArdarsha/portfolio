feaecfadf1e7228109c64680f5baaa91684d8d34
ViduminiArdarsha <ghvidumini@gmail.com>
2025-03-01 13:48:22 -1200

    homescreenname changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,8 +13,8 @@ export default function Home() {
      <Navbar/>
      <section className="flex justify-center gap-40 pt-4 px-20">
        <div className="text-center">
-         <h1 className="font-semibold text-5xl m-8 text-primary"> Vidumini Ardarsha</h1>
-         <h2 className="font-semibold text-7xl m-8 text-accent pt-8">UI/UX Designer</h2>
+         <h1 className="font-semibold text-7xl m-8 text-accent pt-8"> Vidumini Ardarsha</h1>
+         <h2 className="font-semibold text-5xl m-8 text-primary">UI/UX Designer</h2>
          <div className="border-2 border-accent rounded-lg px-6 py-3 text-accent flex justify-center w-30">
           <BiPhoneCall className="cursor-pointer text-2xl"/>
           <a href="#" className="ml-3">Contact Me</a>
